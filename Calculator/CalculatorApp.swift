d3011dd813fd714a42d2cde8244ec873093ca4aa
NovikovValeriy <novikov.valera2005@gmail.com>
2025-02-01 23:59:46 +0300

    Initial commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Валерий Новиков on 1.02.25.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
